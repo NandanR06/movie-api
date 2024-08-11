e4e8178529fe699f7409f8d6518a933839ab9a83
unknown <nandan6102003@gmail.com>
2024-08-11 22:11:22 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from "react";
+import axios from 'axios'
 
 function App() {
+  const [nameVal, setname] = useState([]);
+  // useEffect(() => {
+  //   fetch("https://jsonplaceholder.typicode.com/users")
+  //     .then((res) => {
+  //        let val=res.json()
+  //        console.log(res)
+  //        setname(val)
+  //     }
+  //     )
+  //     .catch((error) => { console.log("error") })
+
+
+  // }, []);
+
+  useEffect(() => {
+    axios.get("https://jsonfakery.com/movies/random/3")
+      .then((res) => {
+        console.log(res)
+        setname(res.data)
+
+      })
+      .catch((err) => { console.log("error") })
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    < div>
+
+      {/* <img src={nameVal[0].poster_path} alt='movie poster' width="200px" /> */}
+      {nameVal.length?(nameVal.map((list) => {
+        return <img src={list.poster_path} alt='movies image' width="200px"/>|| <p>{list.original_title}</p>
+        })):(<p>value not found</p>)
+      }
+      {nameVal.length?(nameVal.map((list) => {
+        return <p>{list.original_title}</p>
+        })):(<p>value not found</p>)
+      }
+
+
     </div>
   );
 }
